4aedc08961dd7ebc7862657a90e28ee9088a53ef
Daniil <51852490+Danill-Superbelka@users.noreply.github.com>
2024-10-11 17:34:05 +0300

    SwiftLint SwiftFormat

---
+++ Time/Time/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Time
+//
+//  Created by Даниил  on 11.10.2024.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
